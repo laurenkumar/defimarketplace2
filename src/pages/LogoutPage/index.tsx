050e7c93e1ac9ebbb4c76e35cb4849aa1577131a
laurenkumar <33550937+laurenkumar@users.noreply.github.com>
2021-12-16 23:10:49 +0100

    Update index.tsx

--- src/pages/LogoutPage/index.tsx
+++ src/pages/LogoutPage/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Spin from 'antd/lib/spin';
-import { getRedirectUrl } from 'src/helpers';
+import { getRedirectUrl } from 'src/utils';
 
 const LogoutPage: React.FC = () => {
   const history = useHistory();
