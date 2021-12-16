fb9cd3ad0f104c4bac1542f1f92f3151b23001c7
laurenkumar <33550937+laurenkumar@users.noreply.github.com>
2021-12-16 23:11:29 +0100

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,6 +7,10 @@ export const serializeQueryString = (obj: Record<string, string>) => {
   return str.join('&');
 };
 
+export const SUPPORTED_CHAINS = [
+  1, 3, 4, 5, 42, 56, 97, 1000, 2000, 80001, 43113, 4002, 888, 999,
+];
+
 export const setRedirectUrl = () => {
   window.localStorage.setItem('redirectUrl', window.location.pathname);
 };
