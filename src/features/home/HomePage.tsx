3049f32b02ef9a76a7411118b9f43aa9be125ce7
laurenkumar <33550937+laurenkumar@users.noreply.github.com>
2021-12-16 21:56:25 +0100

    Update HomePage.tsx

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -9,7 +9,7 @@ const HomePageContent: React.FC = () => {
           <span class='float-right lnr lnr-heart'></span>
         </div>
         <div class='img'>
-          <img src='https://tctechcrunch2011.files.wordpress.com/2014/11/solo2-wireless-red-quarter.jpg?w=738'>
+          <img src='https://tctechcrunch2011.files.wordpress.com/2014/11/solo2-wireless-red-quarter.jpg?w=738' />
         </div>
       </div>
       <div class='card-description'>
@@ -41,7 +41,7 @@ const HomePageContent: React.FC = () => {
           <span class='float-right lnr lnr-heart'></span>
         </div>
         <div class='img'>
-          <img src='https://www.bell.ca/Styles/wireless/all_languages/all_regions/catalog_images/large/iPhoneX_spgry-en_lrg.png'>
+          <img src='https://www.bell.ca/Styles/wireless/all_languages/all_regions/catalog_images/large/iPhoneX_spgry-en_lrg.png' />
         </div>
       </div>
       <div class='card-description'>
@@ -73,7 +73,7 @@ const HomePageContent: React.FC = () => {
           <span class='float-right lnr lnr-heart'></span>
         </div>
         <div class='img'>
-          <img src='https://i.pinimg.com/736x/05/58/c7/0558c796ee706b5cb289ffb68e3b509c--is-the-best-to-the.jpg'>
+          <img src='https://i.pinimg.com/736x/05/58/c7/0558c796ee706b5cb289ffb68e3b509c--is-the-best-to-the.jpg' />
         </div>
       </div>
       <div class='card-description'>
@@ -105,7 +105,7 @@ const HomePageContent: React.FC = () => {
           <span class='float-right lnr lnr-heart'></span>
         </div>
         <div class='img'>
-          <img src='https://www.grootgadgets.com/wp-content/uploads/2017/03/Canon-70-200mm-Lens-mug-White-replica-groot-gadgets-1-400x400.jpg'>
+          <img src='https://www.grootgadgets.com/wp-content/uploads/2017/03/Canon-70-200mm-Lens-mug-White-replica-groot-gadgets-1-400x400.jpg' />
         </div>
       </div>
       <div class='card-description'>
