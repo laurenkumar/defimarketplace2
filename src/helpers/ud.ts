033c93a4b38c69ab5466921507156a26f58e478e
laurenkumar <33550937+laurenkumar@users.noreply.github.com>
2021-12-16 22:23:37 +0100

    Update ud.ts

--- src/helpers/ud.ts
+++ src/helpers/ud.ts
@@ -1,9 +1,17 @@
 import UAuth from '@uauth/js';
 
+const requireEnv = (name: string): string => {
+  const env = process.env[name];
+  if (!env) {
+    throw new Error(`[requireEnv]: ${name} is not set`);
+  }
+  return env;
+};
+
 export default () => {
   return new UAuth({
-    clientID: process.env.REACT_APP_NOT_CLIENT_ID,
-    clientSecret: process.env.REACT_APP_NOT_CLIENT_SECRET,
+    clientID: requireEnv('REACT_APP_NOT_CLIENT_ID'),
+    clientSecret: requireEnv('REACT_APP_NOT_CLIENT_SECRET'),
 
     scope: 'openid email wallet',
 
