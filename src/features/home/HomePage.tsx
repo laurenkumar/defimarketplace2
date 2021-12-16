302203eb3478f2f0f6390814e21108519cc26b65
laurenkumar <33550937+laurenkumar@users.noreply.github.com>
2021-12-16 22:17:36 +0100

    Update HomePage.tsx

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,92 +1,92 @@
 const HomePageContent: React.FC = () => {
   return (
     <>
-      <div class='card'>
-        <div class='card-content'>
-          <div class='top-bar'>
+      <div className='card'>
+        <div className='card-content'>
+          <div className='top-bar'>
             <span>$12.95</span>
-            <span class='float-right lnr lnr-heart'></span>
+            <span className='float-right lnr lnr-heart'></span>
           </div>
-          <div class='img'>
+          <div className='img'>
             <img src='https://tctechcrunch2011.files.wordpress.com/2014/11/solo2-wireless-red-quarter.jpg?w=738' />
           </div>
         </div>
-        <div class='card-description'>
-          <div class='title'>BEATS HEADPHONE</div>
-          <div class='cart'>
-            <span class='lnr lnr-cart'></span>
+        <div className='card-description'>
+          <div className='title'>BEATS HEADPHONE</div>
+          <div className='cart'>
+            <span className='lnr lnr-cart'></span>
           </div>
         </div>
-        <div class='card-footer'>
-          <div class='span'>RED</div>
-          <div class='span'>BEATS</div>
-          <div class='span'>HEADPHONE</div>
+        <div className='card-footer'>
+          <div className='span'>RED</div>
+          <div className='span'>BEATS</div>
+          <div className='span'>HEADPHONE</div>
         </div>
       </div>
-      <div class='card'>
-        <div class='card-content'>
-          <div class='top-bar'>
+      <div className='card'>
+        <div className='card-content'>
+          <div className='top-bar'>
             <span>$1200.95</span>
-            <span class='float-right lnr lnr-heart'></span>
+            <span className='float-right lnr lnr-heart'></span>
           </div>
-          <div class='img'>
+          <div className='img'>
             <img src='https://www.bell.ca/Styles/wireless/all_languages/all_regions/catalog_images/large/iPhoneX_spgry-en_lrg.png' />
           </div>
         </div>
-        <div class='card-description'>
-          <div class='title'>IPHONE X</div>
-          <div class='cart'>
-            <span class='lnr lnr-cart'></span>
+        <div className='card-description'>
+          <div className='title'>IPHONE X</div>
+          <div className='cart'>
+            <span className='lnr lnr-cart'></span>
           </div>
         </div>
-        <div class='card-footer'>
-          <div class='span'>IPHONE</div>
-          <div class='span'>PHONE</div>
-          <div class='span'>MOBILE</div>
+        <div className='card-footer'>
+          <div className='span'>IPHONE</div>
+          <div className='span'>PHONE</div>
+          <div className='span'>MOBILE</div>
         </div>
       </div>
-      <div class='card'>
-        <div class='card-content'>
-          <div class='top-bar'>
+      <div className='card'>
+        <div className='card-content'>
+          <div className='top-bar'>
             <span>$5.95</span>
-            <span class='float-right lnr lnr-heart'></span>
+            <span className='float-right lnr lnr-heart'></span>
           </div>
-          <div class='img'>
+          <div className='img'>
             <img src='https://i.pinimg.com/736x/05/58/c7/0558c796ee706b5cb289ffb68e3b509c--is-the-best-to-the.jpg' />
           </div>
         </div>
-        <div class='card-description'>
-          <div class='title'>Black Shoes</div>
-          <div class='cart'>
-            <span class='lnr lnr-cart'></span>
+        <div className='card-description'>
+          <div className='title'>Black Shoes</div>
+          <div className='cart'>
+            <span className='lnr lnr-cart'></span>
           </div>
         </div>
-        <div class='card-footer'>
-          <div class='span'>SHOES</div>
-          <div class='span'>FORMAL</div>
-          <div class='span'>LEATHER</div>
+        <div className='card-footer'>
+          <div className='span'>SHOES</div>
+          <div className='span'>FORMAL</div>
+          <div className='span'>LEATHER</div>
         </div>
       </div>
-      <div class='card'>
-        <div class='card-content'>
-          <div class='top-bar'>
+      <div className='card'>
+        <div className='card-content'>
+          <div className='top-bar'>
             <span>$44.55</span>
-            <span class='float-right lnr lnr-heart'></span>
+            <span className='float-right lnr lnr-heart'></span>
           </div>
-          <div class='img'>
+          <div className='img'>
             <img src='https://www.grootgadgets.com/wp-content/uploads/2017/03/Canon-70-200mm-Lens-mug-White-replica-groot-gadgets-1-400x400.jpg' />
           </div>
         </div>
-        <div class='card-description'>
-          <div class='title'>Camera Lens</div>
-          <div class='cart'>
-            <span class='lnr lnr-cart'></span>
+        <div className='card-description'>
+          <div className='title'>Camera Lens</div>
+          <div className='cart'>
+            <span className='lnr lnr-cart'></span>
           </div>
         </div>
-        <div class='card-footer'>
-          <div class='span'>CAMERA</div>
-          <div class='span'>GADGET</div>
-          <div class='span'>LENS</div>
+        <div className='card-footer'>
+          <div className='span'>CAMERA</div>
+          <div className='span'>GADGET</div>
+          <div className='span'>LENS</div>
         </div>
       </div>
     </>
